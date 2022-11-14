02976f0966eb948c09097563358f0f70d5bb69f2
Bart Janeczko <bartjaneczko@gmail.com>
2022-11-14 18:51:55 +0100

    added connection to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+connectDB();
 
 const app = express();
 
@@ -16,7 +21,5 @@ app.get("/api/chat/:id", (req, res) => {
   const singleChat = chats.find((c) => c._id === req.params.id);
   res.send(singleChat);
 });
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(`${PORT}`, console.log(`Server running on port ${PORT}`));
