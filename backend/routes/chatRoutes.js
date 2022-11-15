1c3bcbdd208149a43247ce90ca7570d2e9297991
Bart Janeczko <bartjaneczko@gmail.com>
2022-11-15 22:39:19 +0100

    create API for 1-1 chat

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { protect } = require("../middleware/authMiddleware");
+const { accessChat } = require("../controllers/chatControllers");
 
 const router = express.Router();
 
