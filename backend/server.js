64260fc9588a8ead1f85795133487e9ba87d45d3
Bart Janeczko <bartjaneczko@gmail.com>
2022-11-15 10:32:13 +0100

    login API

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -9,17 +10,12 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use("/api/user", userRoutes);
 
 app.listen(`${PORT}`, console.log(`Server running on port ${PORT}`));
