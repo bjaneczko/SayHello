6ed432c7f1659257c9d66c8fda748a0a3dd43c35
Bart Janeczko <bartjaneczko@gmail.com>
2022-11-18 12:30:34 +0100

    created context

---
+++ frontend/src/context/ChatProvider.js
@@ -0,0 +1,9 @@
+import {} from "react";
+
+const ChatContext = createContext();
+
+const ChatProvider = ({ children }) => {
+  return <ChatContext.Provider>{children}</ChatContext.Provider>;
+};
+
+export default ChatProvider;
