184d8f4026c5212a00fdfc34357cb6c2298798ee
Bart Janeczko <bartjaneczko@gmail.com>
2022-11-19 18:16:05 +0100

    added sidebar

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { useRef, useEffect, useCallback } from "react";
+import { useSpring, animated } from "react-spring";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+import {
+  Background,
+  SidebarContent,
+  SidebarWrapper,
+  InputContainer,
+  SearchInput,
+  Button,
+  SearchResultContainer,
+  ResultHeader,
+  ResultText,
+} from "../sidebar/Sidebar.styled";
+
+const Sidebar = ({
+  showSidebar,
+  setShowSidebar,
+  search,
+  setSearch,
+  handleSearch,
+  loading,
+  searchResults,
+  accessChat,
+}) => {
+  const sidebarRef = useRef();
+
+  const animation = useSpring({
+    config: {
+      duration: 250,
+    },
+    opacity: showSidebar ? 1 : 0,
+    transform: showSidebar ? `translateX(0%)` : `translateX(-100%)`,
+  });
+
+  const closeSidebar = (e) => {
+    if (sidebarRef.current === e.target) {
+      setShowSidebar(false);
+    }
+  };
+
+  const keyPress = useCallback(
+    (e) => {
+      if (e.key === "Escape" && showSidebar) {
+        setShowSidebar(false);
+        console.log("I pressed");
+      }
+    },
+    [setShowSidebar, showSidebar]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyPress);
+    return () => document.removeEventListener("keydown", keyPress);
+  }, [keyPress]);
+
+  return (
+    <>
+      {showSidebar ? (
+        <Background onClick={closeSidebar} ref={sidebarRef}>
+          <animated.div style={animation}>
+            <SidebarWrapper showSidebar={showSidebar}>
+              <SidebarContent>
+                <InputContainer>
+                  <SearchInput
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    placeholder="Search by name or email"
+                  />
+                  <Button onClick={handleSearch}>Go</Button>
+                </InputContainer>
+                {loading
+                  ? "Loading..."
+                  : searchResults?.map((user) => (
+                      <SearchResultContainer
+                        key={user._id}
+                        onClick={() => accessChat(user._id)}
+                      >
+                        <ResultHeader>{user.name}</ResultHeader>
+                        <ResultText>{user.email}</ResultText>
+                      </SearchResultContainer>
+                    ))}
+              </SidebarContent>
+            </SidebarWrapper>
+          </animated.div>
+        </Background>
+      ) : null}
+    </>
+  );
 };
 
 export default Sidebar;
---
+++ frontend/src/components/sidebar/Sidebar.styled.js
@@ -0,0 +1,105 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  display: flex;
+`;
+
+export const SidebarWrapper = styled.div`
+  width: 300px;
+  height: 100%;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  color: #000;
+  position: relative;
+  z-index: 10;
+`;
+
+export const SidebarContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.8;
+  color: #141414;
+`;
+
+export const SearchInput = styled.input`
+  position: absolute;
+  left: 10px;
+  background-color: #dde7ee;
+  border: 1px solid transparent;
+  border-radius: 30px;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  font-style: normal;
+  font-weight: 400;
+  padding: 0.75rem 1rem;
+  z-index: 2;
+
+  &:focus,
+  &:active {
+    box-shadow: none;
+    outline: none;
+  }
+`;
+
+export const InputContainer = styled.div`
+  margin-top: 20px;
+  height: 50px;
+  display: flex;
+`;
+
+export const Button = styled.button`
+  position: absolute;
+  right: 10px;
+  background-color: #0083b0;
+  border-radius: 30px;
+  border: none;
+  text-align: center;
+  font-size: 1rem;
+  color: white;
+  padding: 16px 1rem 16px 4rem;
+  z-index: 1;
+
+  &:focus,
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:active {
+    background-color: #007399;
+  }
+`;
+
+export const SearchResultContainer = styled.div`
+  width: 250px;
+  margin-top: 1rem;
+  background-color: #dde7ee;
+  border: 1px solid transparent;
+  border-radius: 30px;
+  padding: 0.75rem 1rem;
+  display: flex;
+  flex-direction: column;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #0083b0;
+    color: white;
+  }
+  &:active {
+    background-color: #007399;
+  }
+`;
+
+export const ResultHeader = styled.div`
+  font-size: 14px;
+  font-weight: 600;
+`;
+
+export const ResultText = styled.div`
+  font-size: 12px;
+`;
