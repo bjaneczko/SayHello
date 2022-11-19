d16a35332f18a0f8759594548fcd6ab4a43b9ba2
Bart Janeczko <bartjaneczko@gmail.com>
2022-11-19 18:10:43 +0100

    del leftover

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -11,22 +11,6 @@ const HomePage = styled.div`
   justify-content: center;
 `;
 
-export const TestUserButton = styled.button`
-  padding-top: 20px;
-  color: white;
-  background: none;
-  border: none;
-  border-bottom: 2px solid white;
-  font-family: inherit;
-  font-size: inherit;
-
-  &:active,
-  &:focus,
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
 const Home = () => {
   const navigate = useNavigate();
 
