c700b8e061cbfbb3db71fd26f5624aa42c5dc251
Bart Janeczko <bartjaneczko@gmail.com>
2022-11-21 14:17:59 +0100

    endpoint

--- frontend/src/components/singleChat/SingleChat.js
+++ frontend/src/components/singleChat/SingleChat.js
@@ -17,7 +17,7 @@ import {
   FormInput,
 } from "./SingleChat.styled";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://say-hello-coo0.onrender.com";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
